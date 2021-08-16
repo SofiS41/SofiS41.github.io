1d90322bc1510023640bb6ab20bde15fb641f8ca
SofiS41 <78041431+SofiS41@users.noreply.github.com>
2021-08-16 08:42:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,23 +7,23 @@
 // — Sign Up: при кліку на дану кнопку відкривається модальне вікно з повідомленням про успішну реєстрацію
 // — Start exploring: при кліку на дану кнопку закривається модальне вікно, а також всі поля форми зачищуються
 
-function checkValue(pattern, element){
+function checkValue(pattern, element) {
     let variable = pattern.test(element.value);
-    if (variable === true){
+    if (variable === true) {
         element.parentElement.classList.add('valid');
         element.classList.remove('shadow');
         element.parentElement.children[1].classList.add('hide');
         element.style.borderColor = 'forestgreen';
-        if(document.querySelector('.alert-message')){
+        if (document.querySelector('.alert-message')) {
             document.querySelector('.alert-message').remove();
         }
     }
-    else{
+    else {
         element.parentElement.classList.remove('valid');
         element.style.borderColor = '#ddd';
         element.parentElement.children[1].classList.remove('hide');
         element.classList.add('shadow');
-        if(document.querySelector('.alert-message') == null){
+        if (document.querySelector('.alert-message') == null) {
             let message = document.createElement('div');
             message.textContent = `Please provide a valid ${element.placeholder}`;
             message.classList.add('alert-message');
@@ -34,40 +34,42 @@ function checkValue(pattern, element){
 }
 
 let form = document.forms.signUpForm;
+let che = document.querySelector('#prpol');
 
 let nameg, surname, email, password;
 let regName = /^[a-zA-Z]{3,20}$/;
-let regUsEmail = /^\w{1,}@[a-z]{2,}\.[a-z]{2,}$/;
+let regUsEmail = /^\S{1,}@[a-z]{2,}\.[a-z]{2,}$/;
 let regPass = /^\w{8,15}$/;
 
-form.addEventListener('input', (e)=>{
-    if(e.target.id === 'fname'){
+form.addEventListener('input', (e) => {
+    if (e.target.id === 'fname') {
         nameg = checkValue(regName, e.target);
     }
-    else if(e.target.id === 'sname'){
+    else if (e.target.id === 'sname') {
         surname = checkValue(regName, e.target);
     }
-    else if(e.target.id === 'usemail'){
+    else if (e.target.id === 'usemail') {
         email = checkValue(regUsEmail, e.target);
     }
-    else if(e.target.id === 'uspassword'){
+    else if (e.target.id === 'uspassword') {
         password = checkValue(regPass, e.target);
     }
-     if(e.target.id === 'prpol'){
-         if (e.target.checked && nameg===true && surname===true && email===true && password===true){
-                document.querySelector('.sign-button').disabled = false;
-            }
-        }
+    if (che.checked === true && nameg === true && surname === true && email === true && password === true) {
+        document.querySelector('.sign-button').disabled = false;
+    }
 });
-document.querySelector('.sign-button').addEventListener('click', ()=>{
+
+console.log(nameg, surname);
+document.querySelector('.sign-button').addEventListener('click', () => {
     document.querySelector('.welcome-overlay').classList.remove('hide');
 });
-document.querySelector('.start-exploring').addEventListener('click', ()=>{
+
+document.querySelector('.start-exploring').addEventListener('click', () => {
     document.querySelector('.welcome-overlay').classList.add('hide');
     form.reset();
-    for(let i=0; i<form.elements.length-2; i++){
+    document.querySelector('.sign-button').disabled = true;
+    for (let i = 0; i < form.elements.length - 2; i++) {
         form.elements[i].parentElement.classList.remove('valid');
         form.elements[i].style.borderColor = '#ddd';
     }
 });
-
